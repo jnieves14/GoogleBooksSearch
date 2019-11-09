4c8ea8368cdaa7ba50dcae45ef8b24dd188f50cd
Jannah June Nieves <jannahjune@gmail.com>
2019-11-08 21:01:58 -0800

    build folder created

---
+++ client/build/precache-manifest.b93442f42ece3b5d9ab199ac65c1fe03.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b433844b5d1539ceee8fd10ae99b8f34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbfcb7e0daf6e1347568",
+    "url": "/static/js/2.e5170e2c.chunk.js"
+  },
+  {
+    "revision": "140e6c2df87f1e26d5c2",
+    "url": "/static/js/main.f169ea98.chunk.js"
+  },
+  {
+    "revision": "7a1ddfe16a5f8d369dfc",
+    "url": "/static/js/runtime-main.54288d36.js"
+  }
+]);
