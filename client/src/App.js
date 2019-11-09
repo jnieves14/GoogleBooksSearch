4fabfe26f0906a59aa74919cefd7a8bfb5f13c12
Jannah June Nieves <jannahjune@gmail.com>
2019-11-08 22:11:28 -0800

    nav showing / add db + controllers

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,20 @@ import {
 import Home from './pages/Home';
 import  Saved from './pages/Saved';
 import NoMatch from './pages/NoMatch';
+import Nav from "./components/Nav";
 
 
 function App() {
   return (
     <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/saved" component={Saved} />
-      <Route component={NoMatch} />
-    </Switch>
+      <div>
+        <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/saved" component={Saved} />
+            <Route component={NoMatch} />
+          </Switch>
+      </div>
   </Router>
   );
 }
