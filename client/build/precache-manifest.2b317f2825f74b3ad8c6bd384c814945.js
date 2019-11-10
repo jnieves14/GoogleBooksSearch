8e099e26cdbddfc8b2501f56bbd3f3f79252c845
Jannah June Nieves <jannahjune@gmail.com>
2019-11-09 23:04:46 -0800

    new npm run build

---
+++ client/build/precache-manifest.2b317f2825f74b3ad8c6bd384c814945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a88b5fad7a108ae27eb5",
+    "url": "/static/css/main.b620413e.chunk.css"
+  },
+  {
+    "revision": "a88b5fad7a108ae27eb5",
+    "url": "/static/js/main.a88b5fad.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fbdb8f483bd540632392",
+    "url": "/static/js/2.fbdb8f48.chunk.js"
+  },
+  {
+    "revision": "68c5b19b41cd28ff6bd0f6880c6851e0",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c047bf5c4fd8d49e4d371efe300d354.js"
+  "/precache-manifest.2b317f2825f74b3ad8c6bd384c814945.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
